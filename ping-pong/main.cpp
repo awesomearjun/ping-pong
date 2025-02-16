e8e3aba51173341e428ab6be8b45ef102b18d506
awesomearjun <iamawesomearjun@gmail.com>
2025-02-16 13:12:36 -0500

    Initial Commit

---
+++ ping-pong/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ping-pong
+//
+//  Created by OnionLord on 2025-02-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
